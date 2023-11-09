36b583d922a968d4ee7c1dffb5ac963b656bacb8
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2023-11-09 09:59:33 -0500

    added eureka server

---
+++ src/main/java/co/crisi/m8commercegoo/M8CommerceGooApplication.java
@@ -0,0 +1,13 @@
+package co.crisi.m8commercegoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M8CommerceGooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M8CommerceGooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/crisi/m8commercegoo/M8CommerceGooApplicationTests.java
@@ -0,0 +1,13 @@
+package co.crisi.m8commercegoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M8CommerceGooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
